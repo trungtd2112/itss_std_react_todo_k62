c8e8834e77f308aa02882f199bbded4d62fa758b
EC2 Default User <ec2-user@ip-172-31-65-193.ec2.internal>
2021-05-18 07:18:51 +0000

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCnbeL4-j73fBuenudaVcgAcxvFeDriueo",
+    authDomain: "trung-fb-sample-cda2e.firebaseapp.com",
+    projectId: "trung-fb-sample-cda2e",
+    storageBucket: "trung-fb-sample-cda2e.appspot.com",
+    messagingSenderId: "1030512747081",
+    appId: "1:1030512747081:web:f3072361faf55c76161598"
+};
+
+
+firebase.initializeApp(firebaseConfig);
